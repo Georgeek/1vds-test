38a6e93533fee3394e88c5c4ad54abe742fd04ba
Egor Madiarov <Georgeek@users.noreply.github.com>
2018-02-07 21:03:03 +0800

    api test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,3 +4,13 @@ import $ from 'jquery';
 $(() => {
 	svg4everybody();
 });
+
+const username = 'root';
+const password = 'VCyZn7mLi6Jk';
+
+const url = `https://82.146.48.90:1500/ispmgr?out=json&func=auth&username=${username}&password=${password}`;
+
+fetch(url)
+	.then((data) => console.log(data));
+
+console.log(url);
