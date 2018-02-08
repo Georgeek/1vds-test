f7cd2f6a87492501b41790989c1f35c2039c60e7
Egor Madiarov <Georgeek@users.noreply.github.com>
2018-02-08 18:28:10 +0800

    api test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,3 +4,25 @@ import $ from 'jquery';
 $(() => {
 	svg4everybody();
 });
+
+const username = 'root';
+const password = 'VCyZn7mLi6Jk';
+let data;
+
+$(document).ready(function(){
+	$.ajax({
+		url: 'https://82.146.48.90:1500/ispmgr',
+		data: {out:'json',func:'auth',username:username,password:password},
+		dataType: 'jsonp',
+
+		/* Very important */
+		contentType: 'application/json',
+		success: (res) => testFunc(res)
+	});
+});
+
+const url = `https://82.146.48.90:1500/ispmgr?func=auth&username=${username}&password=${password}&out=json`;
+
+function testFunc(data) {
+	console.log(data.doc.auth.$id);
+}
